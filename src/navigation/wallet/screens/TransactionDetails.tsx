4a0df86e9fde7c420d7526df9a5bac0dbfc6403d
Gabriel <gabrielbazan89@gmail.com>
2023-06-13 10:53:36 +0200

    [REF] transactions details

--- src/navigation/wallet/screens/TransactionDetails.tsx
+++ src/navigation/wallet/screens/TransactionDetails.tsx
@@ -77,8 +77,8 @@ const TxsDetailsContainer = styled.View`
 `;
 
 const ScrollView = styled(KeyboardAwareScrollView)`
-  margin-top: 20px;
-  padding: 0 ${ScreenGutter};
+  padding: 0px 10px;
+  margin: 20px ${ScreenGutter};
 `;
 
 const SubTitle = styled(BaseText)`
@@ -395,7 +395,7 @@ const TransactionDetails = () => {
 
   return (
     <TxsDetailsContainer>
-      {isLoading && (
+      {!isLoading && txs ? (
         <ScrollView
           keyboardShouldPersistTaps={'handled'}
           extraScrollHeight={80}>
@@ -611,7 +611,7 @@ const TransactionDetails = () => {
             </Button>
           </VerticalSpace>
         </ScrollView>
-      )}
+      ) : null}
     </TxsDetailsContainer>
   );
 };
