9cbbe746871a555dc0ec5f09f1d7f333d2c26660
Gabriel <gabrielbazan89@gmail.com>
2023-07-11 14:20:25 +0200

    [FIX] network fee policy component key warning

--- src/navigation/tabs/settings/NetworkFeePolicy/screens/NewtorkFeePolicy.tsx
+++ src/navigation/tabs/settings/NetworkFeePolicy/screens/NewtorkFeePolicy.tsx
@@ -91,7 +91,7 @@ const FeeOptions = ({
       </StepsHeaderContainer>
 
       {feeOptions.map((fee, i) => (
-        <Setting>
+        <Setting key={i}>
           <SettingTitle>{fee.uiLevel}</SettingTitle>
           <Checkbox
             radio={true}
